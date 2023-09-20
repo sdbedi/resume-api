1e0364480c5b68d7a8f8aac248bd4a0659833db0
Sheel Bedi <sheel.bedi@gmail.com>
2023-09-20 15:08:40 -0500

    more changes

---
+++ cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"resume/config"
+)
+
+func main() {
+	c := config.New()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", hello)
+
+	s := &http.Server{
+		Addr:         fmt.Sprintf(":%d", c.Server.Port),
+		Handler:      mux,
+		ReadTimeout:  c.Server.TimeoutRead,
+		WriteTimeout: c.Server.TimeoutWrite,
+		IdleTimeout:  c.Server.TimeoutIdle,
+	}
+
+	log.Println("Starting server " + s.Addr)
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal("Server startup failed")
+	}
+}
